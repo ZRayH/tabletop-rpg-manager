398b9c80175607a578df5a81194d9184979b1547
Wes Cutting <wesleycutting@gmail.com>
2018-05-08 13:23:55 -0600

    Initial Commit of React Example

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(
+  <div>
+    <App title="Welcome to My App"/>
+    <App title="Look at Props in action"/>
+    <App title="Props allow for data driven Components"/>
+    <App title="Props are immutable"/>
+    <App title="Meaning you can't change their value within the Component"/>
+  </div>,
+  document.getElementById('root'));
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
