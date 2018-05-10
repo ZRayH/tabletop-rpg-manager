719f17fdcd28046ffb8e2720e85c9ca64c37d4ae
Zachary Haynie <zacharyrhaynie@gmail.com>
2018-05-10 14:02:20 -0600

    Spring cleaning
    Good Heavens man, how much more cleaning/reformating of your code do I need to do?

--- src/index.js
+++ src/index.js
@@ -1,21 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
-// import App from './App';
 import Dice from './components/dice/Dice';
-import registerServiceWorker from './registerServiceWorker';
 
-// ReactDOM.render(
-//   <div>
-//     <App title="Welcome to My App"/>
-//     <App title="Look at Props in action"/>
-//     <App title="Props allow for data driven Components"/>
-//     <App title="Props are immutable"/>
-//     <App title="Meaning you can't change their value within the Component"/>
-//   </div>,
-//   document.getElementById('root'));
-
-ReactDOM.render(<Dice />, document.getElementById('root'));
-// ReactDOM.render(<App/>, document.getElementById('root'));
-registerServiceWorker();
-
+ReactDOM.render(<Dice/>, document.getElementById('root'));
