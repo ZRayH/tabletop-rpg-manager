f48c94f6d0a3b1c2abfd41538ea8f7137f700802
Zachary Haynie <zacharyrhaynie@gmail.com>
2018-05-10 13:52:45 -0600

    Clone Wes' repo, Make changes
    -He was limiting me -S/O Josh's scheming mind -Hehehe

--- src/components/dice/Dice.js
+++ src/components/dice/Dice.js
@@ -1,14 +1,9 @@
 import React, { Component, Fragment } from 'react'
 import Die from './Die'
-import Bag from '../data/bag'
+import Bag from '../../data/bag'
 
 export default class extends Component {
-  // state = {
-  //   bag: [ 4, 4, 4, 6, 6, 6, 6, 6, 6, 10, 10, 12, 20, 20 ]
-  // }
-  
   buildDiceBag = (bag) => {
-    console.log("Building DiceBag")
     return bag.map(numSides => <Die numOfSides={numSides}/>)
   }
   
@@ -17,13 +12,8 @@ export default class extends Component {
   }
   
   render() {
-    console.log(Bag)
-    console.log("Rendering Dice")
     const filteredBag = this.filterBag(0)
-    console.log(filteredBag)
     const diceBag = this.buildDiceBag(filteredBag)
-    console.log("DiceBag Built")
-    console.log(diceBag)
     return (
       <div className="flexContainer">
         {diceBag}
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
-import Dice from './game/Dice';
+import Dice from './components/dice/Dice';
 import registerServiceWorker from './registerServiceWorker';
 
 // ReactDOM.render(
